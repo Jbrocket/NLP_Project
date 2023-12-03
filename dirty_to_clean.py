77328aa0978c775dd7454c70fed0ead51bfae692
ppenderg <ppenderg@nd.edu>
2023-12-03 18:50:43 -0500

    sadjioasjdiaso

---
+++ dirty_to_clean.py
@@ -0,0 +1,34 @@
+import re
+
+
+
+
+
+if __name__ == "__main__":
+    infilename = "reddit_dirty.txt"
+    outfilename = "reddit_clean.txt"
+
+    swear_list = ["fuck", "shit", "bitch", "dick", "cunt", "wanker", "pussy", "damn", "ass", "asshole", "bastard", "dickhead", "goddamn"]
+    pattern = swear_list.pop(0)
+    pattern = pattern + " |" + pattern 
+    for swear in swear_list:
+        pattern += "|" + swear + " |" + swear
+
+    # print(pattern)
+
+    infile = open(infilename, "r")
+    outfile = open(outfilename, "w")
+
+    for line in infile:
+        cur_string = ""
+        for char in line:
+            if char == "”" or char == '"' or char == "“" or char == ",":
+                continue
+            else:
+                cur_string += char
+        # cur_string = " ".join(cur_string.split())
+        # cur_string = cur_string.replace(u'\xa0')
+        outfile.write(re.sub(pattern, '', cur_string, flags=re.IGNORECASE))
+
+    infile.close()
+    outfile.close()
